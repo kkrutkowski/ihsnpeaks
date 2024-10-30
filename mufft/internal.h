229333c5cce3e451a379c74847ab1da69ce6ef65
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2024-10-30 16:34:24 +0100

    Add files via upload

---
+++ mufft/internal.h
@@ -0,0 +1,11 @@
+#ifndef MUFFT_INTERNAL_H
+#define MUFFT_INTERNAL_H
+
+#include "./internal/fft_internal.h"
+#include "./internal/fft.h"
+
+#include "./internal/fftc.h"
+#include "./internal/kernelc.h"
+#include "./internal/cpu.h"
+
+#endif
