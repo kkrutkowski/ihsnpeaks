db5481d0188419be202d8d7453d92e613c86057c
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2024-12-01 22:56:56 +0100

    Add files via upload

--- include/simd.h
+++ include/simd.h
@@ -10,12 +10,25 @@
     } m256_union;
 
     typedef union {
-        __m256 data;
+        __m256i data;
         uint32_t values[8];
     } m256i_union;
-    #define VEC m256_union
-    #define IVEC m256i_union
+    typedef m256_union VEC;
+    typedef m256i_union IVEC;
+#else
+    typedef union {
+        uint32_t data;
+        float values[1];
+    } scalar_union;
+
+    typedef union {
+        uint32_t data;
+        uint32_t values[1];
+    } scalari_union;
+    typedef scalar_union VEC;
+    typedef scalari_union IVEC;
 #endif
 
+#define VEC_LEN (sizeof(VEC) / sizeof(float))
 
 #endif // SIMD_H
