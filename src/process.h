35f98fdbd94ebc4bec62f4a2022b126701b481bf
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2025-01-03 04:12:57 +0100

    Add files via upload

---
+++ src/process.h
@@ -0,0 +1,42 @@
+#ifndef PROCESS_H
+#define PROCESS_H
+
+#include "readout.h"
+#include"../include/klib/kstring.h"
+#include"../include/mufft/mufft.x86.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
