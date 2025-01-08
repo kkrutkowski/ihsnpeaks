c53a570ffbde1d9f99d2ff4c3b47a506e4ce811c
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2025-01-09 00:51:21 +0100

    Add files via upload

--- src/process.h
+++ src/process.h
@@ -19,7 +19,6 @@ float correctPower(float K, float nInv) {
     float term1 = ((2.0 * K) - (K * K)) * (0.25 * nInv);
     float term2 = ((24.0 * K) - (132.0 * K * K) + (76.0 * K * K * K) - (9.0 * K * K * K * K)) * (nInv * nInv / 288.0);
     float inside_log = logf(1 + term1 - term2);
-    //printf("%.2f\n", - inside_log); return K;}
 return K - inside_log;}
 
 inline float sabs(complex float z) {return (creal(z) * creal(z)) + (cimag(z) * cimag(z));}
@@ -39,14 +38,13 @@ inline unsigned int custom_dtoa(double v, int size, char *line){
     else {return fast_dtoa(v, new_size, line);}
 }
 
-inline void appendFreq(double freq, float magnitude, int precision, kstring_t* buffer, char* stringBuff) {
+static inline void appendFreq(double freq, float magnitude, int precision, kstring_t* buffer, char* stringBuff) {
     // Convert frequency to string and append to the buffer
-    custom_dtoa(freq, stringBuff);
+    custom_dtoa(freq, precision, stringBuff);
     kputs(stringBuff, buffer);
-    kputc('\t', buffer); // Add a tab character
-
+    kputc('\t', buffer); // Add the separator
     // Convert magnitude to string and append to the buffer
-    custom_ftoa(magnitude, precision, stringBuff);
+    custom_ftoa(magnitude, 2, stringBuff);
     kputs(stringBuff, buffer);
     kputc('\n', buffer); // Add a newline character
 }
@@ -93,40 +91,43 @@ void process_target(char* in_file, buffer_t* buffer, parameters* params){
 
     while(fmin < params->fmax){
 
-        memset(buffer->grids[0], 0, buffer->memBlockSize);
+        for(int t = 0; t < buffer->terms; t++){ //compute the NFFTs required for the data
+            memset(buffer->grids[t], 0, buffer->memBlockSize);
+            double freqFactor = (double)(t+1); //printf("%.2f\n", freqFactor);
 
-        for(uint32_t i = 0; i < buffer->n; i++){
-            double idx_double = buffer->x[i] * fspan; // ok
-            uint32_t idx = (uint32_t)(idx_double); // ok
-            double idx_frac = idx_double - (double)idx; // ok
-            idx = idx % gridLen; // ok
-            fftwf_complex val = cexp(-2.0 * I * M_PI * fmid * buffer->x[i]) * buffer->dy[i]; // ok
-            if (idx_frac > 0.01){
-                float dst = -7.0 - idx_frac; // ok
-                for(uint32_t j = 0; j < 16; j++){//replace with vectorized sinc
-                    buffer->grids[0][idx + j] += val * sin(dst * M_PI) * sin(dst * M_PI / 3) / (dst * dst * M_PI * M_PI / 3); //sinc(x) * sinc(x/3)
-                    dst += 1.0;
+            for(uint32_t i = 0; i < buffer->n; i++){
+                double idx_double = buffer->x[i] * fspan * freqFactor; // ok
+                uint32_t idx = (uint32_t)(idx_double); // ok
+                double idx_frac = idx_double - (double)idx; // ok
+                idx = idx % gridLen; // ok
+                fftwf_complex val = cexp(-2.0 * freqFactor * I * M_PI * fmid * buffer->x[i]) * buffer->dy[i]; // ok
+                if (idx_frac > 0.01){
+                    float dst = -7.0 - idx_frac; // ok
+                    for(uint32_t j = 0; j < 16; j++){//replace with vectorized sinc
+                        buffer->grids[t][idx + j] += val * 3.0f * sinf(dst * M_PI) * sinf(dst * M_PI * 0.33333333f) / (dst * dst * M_PI * M_PI); //sinc(x) * sinc(x/3)
+                        dst += 1.0;
+                    }
+                } else {
+                    buffer->grids[t][idx] += val;
                 }
-            } else {
-                buffer->grids[0][idx] += val;
             }
-        }
-    for(uint32_t j = 0; j < 16; j++){buffer->grids[0][j] += buffer->grids[0][j+gridLen];}
+        for(uint32_t j = 0; j < 16; j++){buffer->grids[t][j] += buffer->grids[t][j+gridLen];}
 
-    fftwf_execute_dft(params->plan, buffer->grids[0], buffer->grids[0]);
+        fftwf_execute_dft(params->plan, buffer->grids[t], buffer->grids[t]);
+    }
 
         for (uint32_t i = shift + 1; i < gridLen; i++) { // negative half
             freq = fmin + ((double)((i) - shift) * invGridLen * fspan); if (freq > params->fmax){goto end;}
             magnitude = sabs(buffer->grids[0][i]);
             magnitude = correctPower(magnitude, nEffInv);
-            if (params->spectrum){appendFreq(freq, magnitude, n, &buffer->spectrum, stringBuff);}
+            if (params->spectrum){appendFreq(freq, magnitude, n, &buffer->spectrum, &stringBuff[0]);}
         }
 
         for (uint32_t i = 0; i <= gridLen * 21 / 64; i++) { // positive half
             freq = fmid + ((double)(i) * invGridLen * fspan);  if (freq > params->fmax){goto end;}
             magnitude = sabs(buffer->grids[0][i]);
             magnitude = correctPower(magnitude, nEffInv);
-            if (params->spectrum){appendFreq(freq, magnitude, n, &buffer->spectrum, stringBuff);}
+            if (params->spectrum){appendFreq(freq, magnitude, n, &buffer->spectrum, &stringBuff[0]);}
         }
         fmin += fjump; fmid += fjump; fmax += fjump;
     } end:
