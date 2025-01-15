879abe45021d1f2b46258738201f5e15bb3dc274
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2025-01-15 14:12:43 +0100

    Add files via upload

--- src/utils/smoother.h
+++ src/utils/smoother.h
@@ -1,6 +1,10 @@
 #ifndef SMOOTHER_H
 #include "readout.h" //for the "peak_t" struct
 
+static inline int32_t wrapidx(int32_t idx, int32_t n){
+        while (idx >= n) {idx -=n;}
+return idx;}
+
 
 
 
