2651726cf4c9d991175294e706e4a8a77b06955b
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2025-01-15 18:22:30 +0100

    Add files via upload

--- src/utils/smoother.h
+++ src/utils/smoother.h
@@ -1,5 +1,21 @@
 #ifndef SMOOTHER_H
+#include "simd.h"
 #include "readout.h" //for the "peak_t" struct
+
+static inline void sortPeaks(peak_t *peaks, int length) {
+    int i, j;
+    peak_t key;
+
+    for (i = 1; i < length; i++) {
+        key = peaks[i];
+        j = i - 1;
+        while (j >= 0 && peaks[j].chi2 > key.chi2) {
+            peaks[j + 1] = peaks[j];
+            j = j - 1;
+        }
+        peaks[j + 1] = key;
+    }
+}
 
 static inline int32_t wrapidx(int32_t idx, int32_t n){
         while (idx >= n) {idx -=n;}
