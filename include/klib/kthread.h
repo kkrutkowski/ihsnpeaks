df4fac2a0f8385838785d9fd7841f57aa142a6a7
Kacper Rutkowski <kk.rutkowski@student.uw.edu.pl>
2025-01-30 15:15:25 +0100

    Add files via upload

--- include/klib/kthread.h
+++ include/klib/kthread.h
@@ -68,6 +68,7 @@ static void *kt_fp_worker(void *data)
     pthread_exit(0);
 }
 
+#ifdef __GLIBC__
 void *kt_forpool_init(int n_threads, bool idle) {
     kt_forpool_t *fp;
     int i;
@@ -115,6 +116,74 @@ void *kt_forpool_init(int n_threads, bool idle) {
     pthread_mutex_unlock(&fp->mutex);
     return fp;
 }
+#else //MUSL
+void *kt_forpool_init(int n_threads, bool idle){
+    kt_forpool_t *fp;
+    int i;
+    fp = (kt_forpool_t*)calloc(1, sizeof(kt_forpool_t));
+    fp->n_threads = fp->n_pending = n_threads;
+    fp->tid = (pthread_t*)calloc(fp->n_threads, sizeof(pthread_t));
+    fp->w = (kto_worker_t*)calloc(fp->n_threads, sizeof(kto_worker_t));
+    for (i = 0; i < fp->n_threads; ++i) fp->w[i].t = fp;
+    pthread_mutex_init(&fp->mutex, 0);
+    pthread_cond_init(&fp->cv_m, 0);
+    pthread_cond_init(&fp->cv_s, 0);
+
+    // Create threads with the appropriate scheduling policy
+    for (i = 0; i < fp->n_threads; ++i) {
+        pthread_attr_t attr;
+        pthread_attr_init(&attr);
+
+        // Set the scheduling policy based on the `idle` flag
+        if (idle) {
+            // Use SCHED_IDLE if the `idle` flag is true
+            struct sched_param param;
+            param.sched_priority = 0; // Priority must be 0 for SCHED_IDLE
+            if (pthread_attr_setschedpolicy(&attr, SCHED_IDLE) != 0) {
+                perror("pthread_attr_setschedpolicy (SCHED_IDLE)");
+                exit(EXIT_FAILURE);
+            }
+            if (pthread_attr_setschedparam(&attr, &param) != 0) {
+                perror("pthread_attr_setschedparam (SCHED_IDLE)");
+                exit(EXIT_FAILURE);
+            }
+        }
+        #ifdef SCHED_BATCH
+        else {
+            // Use SCHED_BATCH if the `idle` flag is false and SCHED_BATCH is defined
+            struct sched_param param;
+            param.sched_priority = 0; // Priority for SCHED_BATCH
+            if (pthread_attr_setschedpolicy(&attr, SCHED_BATCH) != 0) {
+                perror("pthread_attr_setschedpolicy (SCHED_BATCH)");
+                exit(EXIT_FAILURE);
+            }
+            if (pthread_attr_setschedparam(&attr, &param) != 0) {
+                perror("pthread_attr_setschedparam (SCHED_BATCH)");
+                exit(EXIT_FAILURE);
+            }
+        }
+        #endif
+
+        // Ensure the thread inherits the scheduling attributes explicitly
+        if (pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED) != 0) {
+            perror("pthread_attr_setinheritsched");
+            exit(EXIT_FAILURE);
+        }
+
+        // Create the thread
+        if (pthread_create(&fp->tid[i], &attr, kt_fp_worker, &fp->w[i]) != 0) {
+            perror("pthread_create");
+            exit(EXIT_FAILURE);
+        }
+        pthread_attr_destroy(&attr);
+    }
+
+    pthread_mutex_lock(&fp->mutex);
+    while (fp->n_pending) pthread_cond_wait(&fp->cv_m, &fp->mutex);
+    pthread_mutex_unlock(&fp->mutex);
+    return fp;
+}
+#endif
 
 void kt_forpool_destroy(void *_fp)
 {
